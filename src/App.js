dcde38290c3c9affc0e583916a9f4160fbf6acf3
Legendarybob13 <gbogie13@gmail.com>
2022-03-08 12:13:00 -0500

    Remove Unnessisary GA

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useRef, useEffect } from 'react';
 import { useLocation, Switch, Redirect } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
-import ReactGA from 'react-ga';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Layouts
@@ -14,24 +13,14 @@ import Home from './views/homepage';
 import Signup from './components/sections/Signup';
 import Login from './components/sections/Login';
 
-// Initialize Google Analytics
-ReactGA.initialize(process.env.REACT_APP_GA_CODE);
-
-const trackPage = page => {
-  ReactGA.set({ page });
-  ReactGA.pageview(page);
-};
-
 const App = () => {
 
   const childRef = useRef();
   let location = useLocation();
 
   useEffect(() => {
-    const page = location.pathname;
     document.body.classList.add('is-loaded')
     childRef.current.init();
-    trackPage(page);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
